d6e9142b6c781f70c161b0b0150e692f75719537
joaoB <silva.joaobruno@gmail.com>
2016-12-19 20:48:51 +0100

    add views

--- app.js
+++ app.js
@@ -3,8 +3,12 @@
 var express = require('express');
 var app = express();
 
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 app.get('/', function(req, res) {
-  res.send('Billy Jean');
+  res.render('tweets');
 });
 
 
