d48706e2d0152d11017eebdfca1b0a5a2ae3880a
joaoB <silva.joaobruno@gmail.com>
2016-12-26 14:07:57 +0100

    getting tweet from db

--- app.js
+++ app.js
@@ -44,8 +44,20 @@ app.get('/', function(req, res) {
   });
 });
 
+
 app.get('/tweets/:id([0-9]+)/edit', function(req, res) {
-  res.send(req.params.id);
+  var query = 'SELECT * FROM Tweets WHERE id = ?';
+  var id = req.params.id;
+
+  connection.query(query, [id], function(err, results) {
+    if(err || results.length === 0) {
+      console.log(err || 'No tweet found.');
+      res.redirect('/');
+      return;
+    }
+
+    res.render('edit-tweet', { tweet: results[0] });
+  });
 });
 
 
